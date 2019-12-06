5705cd00b206686c75894957f55812811e85c857
chaoswlz <wulize1994@gmail.com>
2019-12-05 22:12:17 -0800

    change test case

--- app/src/androidTest/java/com/example/assignment2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/assignment2/ExampleInstrumentedTest.java
@@ -8,7 +8,12 @@ import androidx.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static com.example.assignment2.FolderUtil.generateFolder;
+import static com.example.assignment2.FolderUtil.getAllFolders;
+import static com.example.assignment2.FolderUtil.getFileRoot;
 import static org.junit.Assert.*;
 
 /**
@@ -23,5 +28,49 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         assertEquals("com.example.assignment2", appContext.getPackageName());
+    }
+
+    @Test
+    public void createFolderTest(){
+        String folderName1 = "test1";
+        String folderName2 = "test2";
+        String folderName3 = "test3";
+        String folderName4 = "test4";
+        String folderName5 = "test4/tt";
+
+        generateFolder(appContext,folderName1);
+        String[] result1 = getAllFolders(appContext);
+        List<String> list = Arrays.asList(result1);
+
+        assertEquals(1,result1.length);
+        assertTrue(list.contains(folderName1));
+
+        generateFolder(appContext,folderName2);
+        generateFolder(appContext,folderName3);
+        String[] result2 = getAllFolders(appContext);
+        list = Arrays.asList(result2);
+        assertEquals(3,result2.length);
+        assertTrue(list.contains(folderName1));
+        assertTrue(list.contains(folderName2));
+        assertTrue(list.contains(folderName3));
+
+        generateFolder(appContext,folderName2);
+        generateFolder(appContext,folderName4);
+        generateFolder(appContext,folderName5);
+        String[] result3 = getAllFolders(appContext);
+        list = Arrays.asList(result3);
+        assertEquals(4,result3.length);
+        assertTrue(list.contains(folderName1));
+        assertTrue(list.contains(folderName2));
+        assertTrue(list.contains(folderName3));
+        assertTrue(list.contains(folderName4));
+
+    }
+
+    @Test
+    public void testgetFileRoot(){
+        String result = getFileRoot(appContext);
+        String expect = "/storage/emulated/0/Android/data/" + appContext.getPackageName() +"/files";
+        assertEquals(expect,result);
     }
 }
--- app/src/test/java/com/example/assignment2/ExampleUnitTest.java
+++ app/src/test/java/com/example/assignment2/ExampleUnitTest.java
@@ -26,62 +26,6 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    Context mMockContext;
-
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void testGenerateAndReadQRcode(){
-        String test1 = "11111";
-        String test2 = "akdja";
-        String test3 = "2019/11/11";
-        String test4 = "http:www.google.com";
-
-        Bitmap bitmap1 = CodeCreator.createQRCode(test1, 400, 400, null);
-        Bitmap bitmap2 = CodeCreator.createQRCode(test2, 400, 400, null);
-        Bitmap bitmap3 = CodeCreator.createQRCode(test3, 400, 400, null);
-        Bitmap bitmap4 = CodeCreator.createQRCode(test4, 400, 400, null);
-
-        Result result1 = parseInfoFromBitmap(bitmap1);
-        Result result2 = parseInfoFromBitmap(bitmap2);
-        Result result3 = parseInfoFromBitmap(bitmap3);
-        Result result4 = parseInfoFromBitmap(bitmap4);
-
-        assertEquals(test1,result1.getText());
-        assertEquals(test2,result2.getText());
-        assertEquals(test3,result3.getText());
-        assertEquals(test4,result4.getText());
-
-
-    }
-
-    public Result parseInfoFromBitmap(Bitmap bitmap) {
-        int[] pixels = new int[bitmap.getWidth() * bitmap.getHeight()];
-        bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-
-        RGBLuminanceSource source = new RGBLuminanceSource(bitmap.getWidth(),
-                bitmap.getHeight(), pixels);
-        GlobalHistogramBinarizer binarizer = new GlobalHistogramBinarizer(source);
-        BinaryBitmap image = new BinaryBitmap(binarizer);
-        Result result = null;
-        try {
-            result = new QRCodeReader().decode(image);
-            return result;
-        } catch (NotFoundException e) {
-            e.printStackTrace();
-        } catch (ChecksumException e) {
-            e.printStackTrace();
-        } catch (FormatException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-
-    }
-
 
 
 }
